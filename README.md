9c1fd7478c0af9ba65e9a3de845fdb88810ad343
helenzhao093 <helen.zhao1026@gmail.com>
2018-03-30 00:56:57 -0400

    added boxplots for prob, features, and similarity:

--- README.md
+++ README.md
@@ -39,4 +39,39 @@ outlined
 2) Colored the cells of datatable according to classification
 3) Styled the datatable
 4) Implemented filters for filtering high TPs, low TNs, and probability range
-5) 
+5) Use noUIslider library to create sliders for filters
+6) Used materialize library to create switches for filtering classifications
+7) Implemented function - when hovering over row of table, draws the path of the
+probabilities on the histograms
+8) Implemented function - when click row of table, added path to the histograms
+
+To-dos
+1) On hover, highlight the table row
+2) Leave the last highlighted row highlighted
+3) Highlight the rows clicked
+4) Legend for classification
+5) Fix styling for filters
+
+Additional Features to consider
+1) Click on a bar to draw paths of all examples in that bar. Allows users to
+see how the probabilities of the examples differ.
+2) Change magnitude of paths to be reflective of # of examples in that path.
+Allows users to see how the algorithm is predicting similar examples and how the
+probabilities of examples differ.
+3) Box plots for feature values. Create box-plot for each feature for each
+classification for each class.
+4) Box plots for TP+FN, TP, FN, TP for each feature for each class. Classes
+are tabbed. Columns have box-plots of the same feature. Rows are box-plots for
+the same classification. Allows users to see the difference in the range
+of feature values for each classification.
+5) Click on a FP bar - bring up box-plots to compare ranges of feature value for
+FP examples and the true class of those examples.
+6) Click on FN bar - bring up box-plots to compare ranges of feature values for
+the FN examples and the true class.
+
+### Week 5:
+1) Added zoom-in feature for probability range
+2) Added similarity measure to datasets
+3) Added boxplots
+4) Opens new window when two stacks are selected, new window shows box plots of
+the probability ranges, feature ranges, and similarity range
--- js/comparison.js
+++ js/comparison.js
@@ -14,7 +14,8 @@ $(document).ready(function(){
   var boxHeight = 30
 
   var boxQuartiles = function (d) { // d is an array
-    var sorted = d.sort()
+    var sorted = d.sort(function(a,b){return a - b})
+    console.log(sorted)
      return [
        d3.quantile(sorted, 0),
        d3.quantile(sorted, 0.25),
@@ -24,27 +25,32 @@ $(document).ready(function(){
      ];
   }
 
-  var constructPlotData = function(groups, dataModel){
-    //[ [{}{}] [{}{}] [{}{}] ]
-    var allData = []
-    dataModel.probColumns.forEach(function(d){
-      allData.push([])
+  var constructPlotData = function(columnNames, groups){
+    var data = []
+    columnNames.forEach(function(name) {
+      data.push([])
     })
-
-    //for (i = 0; i < dataModel.numClasses; i++){
-    dataModel.probColumns.forEach(function(probColumn, j){
+    columnNames.forEach(function(column, j){
       groups.forEach(function(group, i){
-        allData[j].push({
+        data[j].push({
           group: i,
-          name: probColumn,//dataModel.probColumns[i],
+          name: column,//dataModel.probColumns[i],
           quartiles: boxQuartiles(group.map(function(d){ //need to sort
-            return d[probColumn]//d[dataModel.probColumns[i]]
+            return d[column]//d[dataModel.probColumns[i]]
           }))
         })
       })
     })
-    console.log(allData)
-    return allData
+    console.log(data)
+    return data
+  }
+
+  var constructAllPlotData = function(groups, dataModel){
+    return [
+      constructPlotData(dataModel.probColumns, groups),
+      constructPlotData(dataModel.featureColumns, groups),
+      constructPlotData([dataModel.similarity_column], groups)
+    ]
   }
 
   var getBinNum = function(prob){
@@ -69,176 +75,205 @@ $(document).ready(function(){
     console.log(filtered)
     return filtered
   }
+  //legend
+  var legendWidth = 300
+  var legendHeight = 60
+  var legendMargin = {top: 10, left: 20}
+  var legendBoxHeight = 20
+  var legendRowHeight = 25
+  function constructLegend(info){
+    var svg = d3.select(".legend")
+      .append("svg")
+      .attr("width", legendWidth)
+      .attr("height", legendHeight)
 
-  function constructBoxPlots(boxPlotData){
+    for (var i = 0; i < info.length; i++){
+      var row = svg.append("g")
+        .attr("class", "legend-row")
+        .attr("transform", "translate(" + legendMargin.left + "," + (legendMargin.top + info[i].groupNum * legendRowHeight) + ")")
+      row.append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", legendBoxHeight)
+        .attr("height", legendBoxHeight)
+        .style("fill", info[i].color)
+        .attr("strokeWidth", 1)
+        .attr("stroke", "black")
+
+      row.append("text")
+        .attr("x", 20 + 5)
+        .attr("y", 15)
+        .attr("font-family", "Verdana")
+        .attr("font-size", 15)
+        .text("actual:" + info[i].actualClass + "; predicted:" + info[i].predictedClass +  "; bin:" + info[i].binNum)
+    }
+  }
+
+  function constructAllBoxPlots(allPlotData, selectedInfo){
+    constructBoxPlots(allPlotData[0], selectedInfo, ".probability-boxplot-pane")
+    constructBoxPlots(allPlotData[1], selectedInfo, ".feature-boxplot-pane")
+    constructBoxPlots(allPlotData[2], selectedInfo, ".similarity-boxplot-pane")
+  }
+
+  function constructBoxPlots(boxPlotData, selectedInfo, divClassName){
     //[ [{}{}] [{}{}] [{}{}] ]
-    boxPlotData.forEach(function(pairOfData){
-      var title = pairOfData[0].name
-      // get the min and max from each
-      xScaleMin = d3.min(pairOfData[0].quartiles.concat(pairOfData[1].quartiles))
-      xScaleMax = d3.max(pairOfData[0].quartiles.concat(pairOfData[1].quartiles))
-      console.log(xScaleMin, xScaleMax)
-      var xScale = d3.scaleLinear()
-      	.domain([xScaleMin, xScaleMax])
-        .range([0, plotWidth])
+    //var divClassNames = [".probability-boxplot-pane", ".feature-boxplot-pane", ".similarity-boxplot-pane"]
+      boxPlotData.forEach(function(pairOfData){
+        var title = pairOfData[0].name
 
-      var xAxis = d3.axisBottom(xScale).tickFormat(d3.format(".2f"))
+        // get the min and max from each
+        xScaleMin = d3.min(pairOfData[0].quartiles.concat(pairOfData[1].quartiles))
+        xScaleMax = d3.max(pairOfData[0].quartiles.concat(pairOfData[1].quartiles))
+        //console.log(xScaleMin, xScaleMax)
+        var xScale = d3.scaleLinear()
+        	.domain([xScaleMin, xScaleMax])
+          .range([0, plotWidth])
 
-      var yScale = d3.scalePoint()
-        .domain([pairOfData[0].group, pairOfData[1].group])
-        .rangeRound([0, plotHeight - boxHeight])
+        var xAxis = d3.axisBottom(xScale).tickFormat(d3.format(".2f"))
 
-      // setup svg and group that will contain the box plot
-      var svg = d3.select(".probability-boxplot-pane")
-        .append("svg")
-          .attr("width", totalWidth)
-          .attr("height", totalHeight)
-        .append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        var yScale = d3.scalePoint()
+          .domain([pairOfData[0].group, pairOfData[1].group])
+          .rangeRound([0, plotHeight - boxHeight])
 
-      svg.append("text")
-          .attr("class", "title")
-          .attr("x", totalWidth/2 )
-          .attr("y", titleHeight/2 )
-          .attr("textLength", 40)
-          .attr("font-family", "Verdana")
-          .attr("font-size", 15)
-          .text(title)
-          //.attr("fill", function(d) { return color(d.className)})
+        // setup svg and group that will contain the box plot
+        console.log(divClassName)
+        var svg = d3.select(divClassName)
+          .append("svg")
+            .attr("width", totalWidth)
+            .attr("height", totalHeight)
+          .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-      svg.append("g")
-        .attr("class", "x-axis")
-        .attr("transform", "translate(" + boxMargin.left + "," + (plotHeight + titleHeight + margin.top + boxMargin.top) + ")")
-        .call(xAxis)
+        svg.append("text")
+            .attr("class", "title")
+            .attr("x", plotWidth/2 )
+            .attr("y", titleHeight/2 )
+            .attr("textLength", 50)
+            .attr("font-family", "Verdana")
+            .attr("font-size", 15)
+            .text(title)
+            //.attr("fill", function(d) { return color(d.className)})
 
-      // setup group the boxplot elements will render in
-      var g = svg.append("g")
-        .attr("transform", "translate(" + boxMargin.left + "," + (boxMargin.top + titleHeight) + ")");
+        svg.append("g")
+          .attr("class", "x-axis")
+          .attr("transform", "translate(" + boxMargin.left + "," + (plotHeight + titleHeight + margin.top + boxMargin.top) + ")")
+          .call(xAxis)
 
-      var horizontalLines = g.selectAll(".horizontal-line")
-          .data(pairOfData)
-          .enter()
-        .append("line")
-          .attr("x1", function(d){
-        	   return xScale(d.quartiles[0])
-           })
-          .attr("y1", function(d){
-        	   return yScale(d.group) + boxHeight/2
-           })
-          .attr("x2", function(d){
-        	   return xScale(d.quartiles[4])
-           })
-          .attr("y2", function(d){
-        	   return yScale(d.group) + boxHeight/2
-           })
-          .attr("stroke", "black")
-          .attr("stroke-width", 1)
+        // setup group the boxplot elements will render in
+        var g = svg.append("g")
+          .attr("transform", "translate(" + boxMargin.left + "," + (boxMargin.top + titleHeight) + ")");
 
-      var rects = g.selectAll("rect")
-          .data(pairOfData)
-          .enter()
-        .append("rect")
-          .attr("width", function(d){
-          	//console.log(d.quartile[2] - d.quartile[0])
-          	//console.log(xScale(2))
-            console.log(d.quartiles[3] - d.quartiles[1], xScale(d.quartiles[3]) - xScale(d.quartiles[1]))
-            return xScale(d.quartiles[3]) - xScale(d.quartiles[1]);
-          })
-          .attr("height", boxHeight)
-          .attr("x", function(d){
-          	return xScale(d.quartiles[1]); //box starts at 1/4 quartile
-          })
-          .attr("y", function(d){
-          	return yScale(d.group)
-          })
-          .attr("fill", "red")
-          .attr("stroke", "black")
-
-      var whiskers = [
-        //left whisker, min value
-        {
-          x1: function(d) { return xScale(d.quartiles[0]) },
-          y1: function(d) { return yScale(d.group) },
-          x2: function(d) { return xScale(d.quartiles[0]) },
-          y2: function(d) { return yScale(d.group) + boxHeight }
-        },
-        {
-          x1: function(d) { return xScale(d.quartiles[2]) },
-          y1: function(d) { return yScale(d.group) },
-          x2: function(d) { return xScale(d.quartiles[2]) },
-          y2: function(d) { return yScale(d.group) + boxHeight }
-        },
-        {
-          x1: function(d) { return xScale(d.quartiles[4]) },
-          y1: function(d) { return yScale(d.group) },
-          x2: function(d) { return xScale(d.quartiles[4]) },
-          y2: function(d) { return yScale(d.group) + boxHeight }
-        }
-      ]
-
-      for (i = 0; i < whiskers.length; i++){
-        var whiskerConfig = whiskers[i];
-        var horizontalLine = g.selectAll(".whiskers")
+        var horizontalLines = g.selectAll(".horizontal-line")
             .data(pairOfData)
             .enter()
           .append("line")
-            .attr("x1", whiskerConfig.x1)
-            .attr("y1", whiskerConfig.y1)
-            .attr("x2", whiskerConfig.x2)
-            .attr("y2", whiskerConfig.y2)
+            .attr("x1", function(d){
+          	   return xScale(d.quartiles[0])
+             })
+            .attr("y1", function(d){
+          	   return yScale(d.group) + boxHeight/2
+             })
+            .attr("x2", function(d){
+          	   return xScale(d.quartiles[4])
+             })
+            .attr("y2", function(d){
+          	   return yScale(d.group) + boxHeight/2
+             })
             .attr("stroke", "black")
             .attr("stroke-width", 1)
-      }
+
+        var rects = g.selectAll("rect")
+            .data(pairOfData)
+            .enter()
+          .append("rect")
+            .attr("width", function(d){
+            	//console.log(d.quartile[2] - d.quartile[0])
+            	//console.log(xScale(2))
+              console.log(d.quartiles)
+              console.log(Math.abs(d.quartiles[3]) - d.quartiles[1], xScale(d.quartiles[3]) - xScale(d.quartiles[1]))
+              return xScale(d.quartiles[3]) - xScale(d.quartiles[1]);
+            })
+            .attr("height", boxHeight)
+            .attr("x", function(d){
+            	return xScale(d.quartiles[1]); //box starts at 1/4 quartile
+            })
+            .attr("y", function(d){
+            	return yScale(d.group)
+            })
+            .attr("fill", function(d){
+              for (var i = 0; i < selectedInfo.length; i++){
+                //console.log(selectedInfo[i].groupNum, d.group)
+                if (selectedInfo[i].groupNum == d.group){
+                  return selectedInfo[i].color
+                }
+              }
+            })
+            .attr("stroke", "black")
+
+        var whiskers = [
+          //left whisker, min value
+          {
+            x1: function(d) { return xScale(d.quartiles[0]) },
+            y1: function(d) { return yScale(d.group) },
+            x2: function(d) { return xScale(d.quartiles[0]) },
+            y2: function(d) { return yScale(d.group) + boxHeight }
+          },
+          {
+            x1: function(d) { return xScale(d.quartiles[2]) },
+            y1: function(d) { return yScale(d.group) },
+            x2: function(d) { return xScale(d.quartiles[2]) },
+            y2: function(d) { return yScale(d.group) + boxHeight }
+          },
+          {
+            x1: function(d) { return xScale(d.quartiles[4]) },
+            y1: function(d) { return yScale(d.group) },
+            x2: function(d) { return xScale(d.quartiles[4]) },
+            y2: function(d) { return yScale(d.group) + boxHeight }
+          }
+        ]
+
+        for (i = 0; i < whiskers.length; i++){
+          var whiskerConfig = whiskers[i];
+          var horizontalLine = g.selectAll(".whiskers")
+              .data(pairOfData)
+              .enter()
+            .append("line")
+              .attr("x1", whiskerConfig.x1)
+              .attr("y1", whiskerConfig.y1)
+              .attr("x2", whiskerConfig.x2)
+              .attr("y2", whiskerConfig.y2)
+              .attr("stroke", "black")
+              .attr("stroke-width", 1)
+        }
+      })
+    }
+
+  var modifySelectedInfo = function(selectedInfo, boxPlotData){
+
+    var colors = []
+    selectedInfo.forEach(function(info, i){
+      var colorIndex = settings.allClassNames.indexOf("class" + info["actualClass"])
+      colors.push(settings.colorRange[colorIndex])
     })
+    console.log(colors)
+
+    selectedInfo.forEach(function(d, i){
+      d.groupNum = i,
+      d.color = colors[i]
+    });
+    console.log(selectedInfo)
   }
 
   var makeComparison = function(selectedInfo, histogramData, data){
     var group1 = filterDataForSelected(data, selectedInfo[0])
     var group2 = filterDataForSelected(data, selectedInfo[1])
 
-    var boxPlotData = constructPlotData([group1,group2], dataModel)
-    constructBoxPlots(boxPlotData)
+    var boxPlotData = constructAllPlotData([group1,group2], dataModel)
+
+    modifySelectedInfo(selectedInfo, boxPlotData)
+    constructLegend(selectedInfo)
+    constructAllBoxPlots(boxPlotData, selectedInfo)
   }
 
   makeComparison(selectedInfo, histogramData, dataModel.data)
 })
-
-
-/*var rects = g.selectAll("rect")
-	.data(boxData)
-  .enter()
-  .append("rect")
-  .attr("width", function(d){
-  	console.log(d.quartile[2] - d.quartile[0])
-  	console.log(xScale(2))
-    return xScale(d.quartile[2] - d.quartile[0]);
-  })
-  .attr("height", "50")
-  .attr("x", function(d){
-  	return xScale(d.quartile[0]);
-  })
-  .attr("y", function(d){
-  	return yScale(d.num)
-  })
-  .attr("fill", "red")
-  .attr("stroke", "black")
-
-  var vertLine = [
-  {
-  	x1:function(d) { return xScale(d.min)},
-    y1:function(d) { return yScale(d.num) - 25},
-    x2:function(d) { return xScale(d.min)},
-    y2:function(d) { return yScale(d.num) + 25},
-  }
-  ]
-
-  var verLin = g.selectAll(".whisker")
-  	.data(boxData)
-    .enter()
-    .append("line")
-    .attr("x1", vertLine[0].x1)
-    .attr("y1", vertLine[0].y1)
-    .attr("x2", vertLine[0].x2)
-    .attr("y2", vertLine[0].y2)
-    .attr("stroke", "black")
-    .attr("stroke-width", 1)*/
